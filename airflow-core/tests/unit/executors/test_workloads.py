04225f52b38db5374b05449417e7ebbaea5350aa
wjddn279 <ehwjddn279@naver.com>
2026-04-26 15:07:33 +0900

    fix logic

--- airflow-core/tests/unit/executors/test_workloads.py
+++ airflow-core/tests/unit/executors/test_workloads.py
@@ -35,8 +35,6 @@ from airflow.models.callback import CallbackKey
 
 
 def test_task_instance_alias_keeps_backwards_compat():
-    from airflow.executors import workloads
-
     assert TaskInstance is TaskInstanceDTO
     assert workloads.TaskInstance is TaskInstanceDTO
     assert workloads.TaskInstanceDTO is TaskInstanceDTO
